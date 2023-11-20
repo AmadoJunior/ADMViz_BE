26a0ae62c264f14cd4ccb659916972d6d91035e4
AmadoJunior <amadoelrepalol@gmail.com>
2023-11-20 18:17:55 -0500

    Front-End Changes

---
+++ src/main/app/src/Context/UserDetailsContext/interfaces.ts
@@ -0,0 +1,26 @@
+enum Role {
+  ADMIN = "ROLE_ADMIN",
+  USER = "ROLE_USER",
+}
+
+export interface IAuthority {
+  authority: Role
+}
+
+export interface IUserDetails {
+  username: string,
+  enabled: boolean,
+  authorities: IAuthority[],
+  accountNonExpired: boolean,
+  accountNonLocked: boolean,
+  credentialsNonExpired: boolean,
+}
+
+export interface IUserDetailsContext {
+  userDetails?: IUserDetails,
+  isAuthenticated: boolean,
+  errored: boolean,
+  handleIsAuthenticated: () => Promise<void>,
+  clearAuthentication: () => void,
+  handleErrored: (isErrored: boolean) => void,
+}
---
+++ src/main/app/src/Context/UserDetailsContext/useUserDetailsContext.tsx
@@ -0,0 +1,83 @@
+//Deps
+import {createContext, useState, useEffect} from "react";
+
+//Interfaces
+import { IUserDetailsContext, IAuthority, IUserDetails } from "./interfaces";
+import { isErrored } from "stream";
+
+//Context
+export const UserDetailsContext = createContext<IUserDetailsContext>({
+  isAuthenticated: false,
+  errored: false,
+  handleIsAuthenticated: (): Promise<void> => { 
+    return new Promise((resolve, reject) => reject())
+  },
+  clearAuthentication: (): void => {},
+  handleErrored: (isErrored: boolean): void => {},
+});
+
+export const useUserDetailsContext = (): IUserDetailsContext => {
+  //State
+  const [userDetails, setUserDetails] = useState<IUserDetails | undefined>();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [errored, setErrored] = useState(false);
+
+  //Methods
+  const handleIsAuthenticated = (): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      //Fetch User Details
+      const basePath = "http://localhost:8080";
+      fetch(`${basePath}/api/self`, {
+        method: "GET"
+      })
+      .then(res => {
+        console.log(res);
+        if(res.status !== 200) throw new Error("Could Not Get User Details RES");
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        if(!data) throw new Error("Could Not Get User Details JSON");
+        setUserDetails(data);
+        //Set Is Auth
+        setIsAuthenticated(true);
+        setErrored(false);
+        resolve();
+      })
+      .catch(e => {
+        console.error(e);
+        //Set State
+        setUserDetails(undefined);
+        setIsAuthenticated(false);
+        reject(e);
+      })
+    })
+  }
+
+  const clearAuthentication = (): void => {
+    setUserDetails(undefined);
+    setIsAuthenticated(false);
+  }
+
+  const handleErrored = (isErrored: boolean): void => {
+    setErrored(isErrored);
+  }
+
+  useEffect(() => {
+    try{
+      handleIsAuthenticated();
+    } catch(e) {
+      console.error(e);
+    }
+  }, [])
+
+  useEffect(() => {
+    if(errored) {
+      clearAuthentication();
+    }
+  }, [errored])
+
+  return {userDetails, isAuthenticated, errored, handleIsAuthenticated, handleErrored, clearAuthentication};
+}
+
+export default useUserDetailsContext;
