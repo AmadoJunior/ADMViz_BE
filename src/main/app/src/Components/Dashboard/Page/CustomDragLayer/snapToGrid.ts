9498b751a75db941f1563b97c2bf90a301b3b7c9
AmadoJunior <amadoelrepalol@gmail.com>
2024-01-12 23:04:27 -0500

    - Removed FE - Secured App Props

--- src/main/app/src/Components/Dashboard/Page/CustomDragLayer/snapToGrid.ts
+++
@@ -1,5 +0,0 @@
-export function snapToGrid(x: number, y: number, stepSize: number): [number, number] {
-  const snappedX = Math.round(x / stepSize) * stepSize
-  const snappedY = Math.round(y / stepSize) * stepSize
-  return [snappedX, snappedY]
-}
