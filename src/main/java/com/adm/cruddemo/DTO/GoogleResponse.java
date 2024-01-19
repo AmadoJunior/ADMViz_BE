b984cf2c2a3477d2fadf653d689012f54b935b41
AmadoJunior <amadoelrepalol@gmail.com>
2024-01-18 19:59:16 -0500

    - Added logger for debugging

--- src/main/java/com/adm/cruddemo/DTO/GoogleResponse.java
+++ src/main/java/com/adm/cruddemo/DTO/GoogleResponse.java
@@ -2,6 +2,7 @@ package com.adm.cruddemo.DTO;
 
 import com.fasterxml.jackson.annotation.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +14,7 @@ import java.util.Map;
         "hostname",
         "error-codes"
 })
+
 public class GoogleResponse {
 
     @JsonProperty("success")
@@ -92,5 +94,15 @@ public class GoogleResponse {
 
     public void setErrorCodes(ErrorCode[] errorCodes) {
         this.errorCodes = errorCodes;
+    }
+
+    @Override
+    public String toString() {
+        return "GoogleResponse{" +
+                "success=" + success +
+                ", challengeTs='" + challengeTs + '\'' +
+                ", hostname='" + hostname + '\'' +
+                ", errorCodes=" + Arrays.toString(errorCodes) +
+                '}';
     }
 }
--- src/main/java/com/adm/cruddemo/service/CaptchaService.java
+++ src/main/java/com/adm/cruddemo/service/CaptchaService.java
@@ -2,7 +2,10 @@ package com.adm.cruddemo.service;
 
 import com.adm.cruddemo.DTO.GoogleResponse;
 import com.adm.cruddemo.captcha.CaptchaSettings;
+import com.adm.cruddemo.controller.AuthenticationController;
 import com.adm.cruddemo.exception.InvalidReCaptchaException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -16,6 +19,8 @@ public class CaptchaService {
     private CaptchaSettings captchaSettings;
     @Autowired
     private RestOperations restTemplate;
+    private Logger logger = LoggerFactory.getLogger(CaptchaService.class);
+
     public void processToken(String captchaToken) throws InvalidReCaptchaException {
         if(!responseSanityCheck(captchaToken)) {
             throw new InvalidReCaptchaException("Response contains invalid characters");
@@ -26,6 +31,7 @@ public class CaptchaService {
                 captchaSettings.getSecret(), captchaToken));
 
         GoogleResponse googleResponse = restTemplate.getForObject(verifyUri, GoogleResponse.class);
+        logger.debug(googleResponse.toString());
 
         if(!googleResponse.isSuccess()) {
             throw new InvalidReCaptchaException("reCaptcha was not successfully validated");
